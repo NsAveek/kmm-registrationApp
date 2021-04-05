a4929ebe4bb5a43b04432eb760f460801e3c8825
Aveek <nsaveek@gmail.com>
2021-04-05 16:27:38 +0800

    Add necessary files

---
+++ shared/src/commonMain/kotlin/com/example/expensetracker/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.example.expensetracker.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/expensetracker/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.expensetracker.shared
+
+expect class Platform() {
+    val platform: String
+}
